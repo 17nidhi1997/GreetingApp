26f6a7cd3b2feafa84ef164f9073bdaae8fef287
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-28 22:09:54 +0530

    [Srinidhi]Add: added Entity Framework and repository

---
+++ GreetingAppRepositoriesLayer/RepositoryImplemantion/IGreetingRepository.cs
@@ -0,0 +1,16 @@
+﻿using GreetingAppCommannLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreetingAppRepositoriesLayer
+{
+    public interface IGreetingRepository
+    {
+        GreetingModel GetGreetingModel(int EmployeeId);
+        IEnumerable<GreetingModel> GetAllGreetingModels();
+        GreetingModel Add(GreetingModel employee);
+        GreetingModel Update(GreetingModel employeeChanges);
+        GreetingModel Delete(int EmployeeId);
+    }
+}
