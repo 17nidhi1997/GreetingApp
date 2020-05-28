5d1d5c9752e23ab4a17847d25ffb0a9d03b4fe03
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-28 22:03:25 +0530

    [Srinidhi]Add: added Entity Framework and DB context

---
+++ GreetingAppRepositoriesLayer/Context/RepoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GreetingAppCommannLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreetingAppRepositoriesLayer.Context
+{
+    public class RepoDbContext: DbContext
+    {
+        public RepoDbContext(DbContextOptions<RepoDbContext> options) : base(options)
+        {
+        }
+        public DbSet<GreetingModel> Employee { get; set; }
+        
+    }
+}
