073f0949a11941ae17e7ac7b65c3063b94e5a011
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-30 08:36:51 +0530

    [Srinidhi]Added: Greeting App for Employee details

--- GreetingApp/Startup.cs
+++ GreetingApp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using GreetingAppRepositoriesLayer.Context;
 using Microsoft.AspNetCore.Mvc;
+using GreetingAppRepositoriesLayer;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace GreetingApp
 {
@@ -25,19 +27,32 @@ namespace GreetingApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContextPool<RepoDbContext>(
-                options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+            options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            
-
+            services.AddTransient<IGreetingRepository, GreetingRepository>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "GreetingAdd", Version = "v1" });            
+            });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json","GreetingApp v1");
+                });
             }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseMvc();
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.Run(async (context) =>
