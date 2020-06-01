cf4ca064a421fff2a68c2537ce80a287dc281716
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-01 11:11:06 +0530

    [Srinidhi]Added: updated the manager interface

---
+++ GreetingAppManagerLayer/Manager/IGreetingManager.cs
@@ -0,0 +1,20 @@
+﻿using GreetingAppCommannLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingAppManagerLayer.Manager
+{
+    public interface IGreetingManager
+    {
+        IEnumerable<GreetingModel> GetAllGreetingModels();
+        GreetingModel GetGreetingModel(int EmployeeId);
+        Task<int> Add(GreetingModel employee);
+        void Update(GreetingModel employeeChanges);
+        GreetingModel Delete(int Employee);
+        bool Login(string username, string password);
+        void save();
+       
+    }
+}
