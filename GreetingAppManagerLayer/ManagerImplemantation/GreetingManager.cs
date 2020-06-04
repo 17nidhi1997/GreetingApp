72102bc043634e8d5670ea1130697bd49b92aa45
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-04 11:09:15 +0530

    [Srinidhi]Added: Employee details using CRUD operation using Ajax

---
+++ GreetingAppManagerLayer/ManagerImplemantation/GreetingManager.cs
@@ -0,0 +1,42 @@
+﻿using GreetingAppCommannLayer;
+using GreetingAppRepositoriesLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingAppManagerLayer.ManagerImplemantation
+{
+    public class GreetingManager : IGreetingManager
+    {
+        private IGreetingRepository _repository;
+        public GreetingManager(IGreetingRepository repository)
+        {
+            _repository = repository;
+        }
+        public Task<int> AddEmployee(GreetingModel employee)
+        {
+            return this._repository.AddEmployee(employee);
+        }
+        public GreetingModel DeleteEmployee(int Employee)
+        {
+            return this._repository.DeleteEmployee(Employee);
+        }
+        public IEnumerable<GreetingModel> GetAllGreetingModels()
+        {
+            return this._repository.GetAllGreetingModels();
+        }
+        public GreetingModel GetGreetingModel(int EmployeeId)
+        {
+            return this._repository.GetGreetingModel(EmployeeId);
+        }       
+        public void saveEmployee()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<int> UpdateEmployee(GreetingModel employeeChanges)
+        {
+            return this._repository.UpdateEmployee(employeeChanges);
+        }
+    }
+}
