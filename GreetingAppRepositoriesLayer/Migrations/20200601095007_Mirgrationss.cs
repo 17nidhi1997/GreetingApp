f2180e9f3cd06fc17d63daaaf9fbc40adde4f691
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-04 11:14:01 +0530

    [Srinidhi]Added: Employee details using CRUD operation using Ajax

---
+++ GreetingAppRepositoriesLayer/Migrations/20200601095007_Mirgrationss.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GreetingAppRepositoriesLayer.Migrations
+{
+    public partial class Mirgrationss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(maxLength: 20, nullable: false),
+                    EmailId = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    MobileNumber = table.Column<string>(maxLength: 11, nullable: false),
+                    Salary = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
